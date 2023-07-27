c04a911872457c752d8379234db85752db419e91
Tomoki Chiku <123437215+tolavo213@users.noreply.github.com>
2023-07-27 19:56:30 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+jQuery(function ($) {
+  // ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $("body").toggleClass("fixed");
+    $(".js-drawer").fadeToggle();
+  });
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "fade",
+  speed: 3000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+$(function () {
+  $(".js-accordion__item:first-child .js-accordion__content").css(
+    "display",
+    "block"
+  );
+  $(".js-accordion__item:first-child .js-accordion__title").addClass("is-open");
+  $(".js-accordion__title").on("click", function () {
+    $(this).toggleClass("is-open");
+    $(this).next().slideToggle(300);
+  });
+});
